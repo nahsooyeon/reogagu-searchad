93a19c3efc33fea81796fddcd6cb6ae41bb4fca6
nahsooyeon <nahsooyeon@gmail.com>
2021-08-02 14:32:03 +0900

    🎉 셋업: express+typescript+next

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from "express";
+import next from "next";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== "produciton";
+const app = next({ dev });
+
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    await app.prepare();
+    const server = express();
+    server.all("*", (req: Request, res: Response) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IndexPage() {
+  return (<div>Next - Typescript - Express</div>);
+}
+
+export default IndexPage;
