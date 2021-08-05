2a78b5541b08ff18d7153bda7636c1d5514ef45d
nahsooyeon <nahsooyeon@gmail.com>
2021-08-05 10:20:00 +0900

    🔨 추가:광고채널별  api 요청 코드 파일

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import next from "next";
 import * as dotenv from "dotenv";
+import { Authentication } from "./google/authentification";
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const dev = process.env.NODE_ENV !== "produciton";
-const app = next({ dev });
+/*const dev = process.env.NODE_ENV !== "production";
+ const app = next({ dev });
 
 const handle = app.getRequestHandler();
-const port = process.env.PORT || 3000;
+
 
 (async () => {
   try {
@@ -25,3 +27,31 @@ const port = process.env.PORT || 3000;
     process.exit(1);
   }
 })();
+ */
+
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+}
+
+const server = new Server().app;
+server.set("port", PORT);
+
+server
+  .listen(server.get("port"), () => {
+    console.log(`${server.get("port")} server is Running`);
+  })
+  .on("error", (err) => {
+    console.log(`Error Message ${err}`);
+  });
+
+server.get("/", async (req, res) => {
+  try {
+    const { sheets } = await Authentication();
+    const response = await sheets.spreadsheets.values.get({})
+  } catch (e) {}
+});
