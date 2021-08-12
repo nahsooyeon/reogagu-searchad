3e00c95463a90fb4a75c29bf16996069f6d3f9bc
nahsooyeon <nahsooyeon@gmail.com>
2021-08-12 15:28:17 +0900

    🙈 수정: gitignore에 google api 키 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function IndexPage() {
-  return (<div>Next - Typescript - Express</div>);
+  return (<div>레오가구 - 네이버/카카오 광고 데이터 받아오기</div>);
 }
 
 export default IndexPage;
