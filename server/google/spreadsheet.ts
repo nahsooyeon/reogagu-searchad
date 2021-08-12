e8febfee41e8d2f40d68d7f81bb7914fafdf2355
nahsooyeon <nahsooyeon@gmail.com>
2021-08-12 18:01:44 +0900

    🔨 추가: 구글 스프레드 시트 샘플 코드

---
+++ server/google/spreadsheet.ts
@@ -0,0 +1,21 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
+const doc = new GoogleSpreadsheet("<the sheet ID from the url>");
+
+// Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+await doc.useServiceAccountAuth({
+  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+});
+
+await doc.loadInfo(); // loads document properties and worksheets
+console.log(doc.title);
+await doc.updateProperties({ title: "renamed doc" });
+
+const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+console.log(sheet.title);
+console.log(sheet.rowCount);
+
+// adding / removing sheets
+const newSheet = await doc.addSheet({ title: "hot new sheet!" });
+await newSheet.delete();
